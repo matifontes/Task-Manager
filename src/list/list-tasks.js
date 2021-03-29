15f457224c0eb7664f2d045911be67602fe440d8
Matias Fontes <matifontes13@gmail.com>
2021-03-29 20:56:08 -0300

    Create html to list tasks

--- src/list/list-tasks.js
+++ src/list/list-tasks.js
@@ -1,12 +1,33 @@
 import React from 'react';
 import { A } from 'hookrouter';
+import { Table } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function ListTasks() {
   return (
-    <A href="/cadastrar"
-      className="btn btn-success btn-sm">
-      New task
-    </A>
+    <div className="text-center">
+      <h3> Task to do </h3>
+      <Table striped bordered hover responsive data-testid="table" >
+        <thead>
+          <tr>
+            <th> Task </th>
+            <th>
+              <A href="/register"
+                className="btn btn-success btn-sm"
+                data-testid="btn-newTask">
+                <FontAwesomeIcon icon={faPlus}/>
+                &nbsp;
+                New Task
+                </A>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+      </Table>
+    </div>
   );
 }
 
