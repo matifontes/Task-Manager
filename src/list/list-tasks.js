062f154a95d092741473ed2e71900303994bae86
Matias Fontes <matifontes13@gmail.com>
2021-03-29 21:22:47 -0300

    useState and useEffect in listTasks

--- src/list/list-tasks.js
+++ src/list/list-tasks.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { A } from 'hookrouter';
 import { Table } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 
 function ListTasks() {
+
+  const [tasks, setTasks] = useState([]);
+  const [loadTasks, setLoadTasks] = useState(true);
+
+
+  useEffect(() => {
+    function getTasks(){
+      const tasksDb = localStorage['tasks'];
+      let listTasks = tasksDb ? JSON.parse(tasksDb) : [];
+      setTasks(listTasks);
+    }
+    if (loadTasks) {
+      getTasks();
+      setLoadTasks(false);
+    }
+  }, [loadTasks]);
+
   return (
     <div className="text-center">
       <h3> Task to do </h3>
