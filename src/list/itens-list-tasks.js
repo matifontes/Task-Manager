d9f8503f2ef723088dfa4407921a7c3dbb9f1bcb
Matias Fontes <matifontes13@gmail.com>
2021-03-29 21:53:06 -0300

    create html

--- src/list/itens-list-tasks.js
+++ src/list/itens-list-tasks.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
+import { A } from 'hookrouter';
 
 function ItemsListTasks(props) {
 
-  return();
+  return{
+    props.tasks.map(tasks =>
+      <tr key ={task.id} data-testid="task">
+        <td width="75%"
+          data-testid = "name-task">
+          {task.name}
+        </td>
+      </tr>
+    );
+  }
 
 }
 
